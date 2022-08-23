c69d6cdf6c9f1135c0b998bfc980325f6f6c5f52
Ali-m24 <ali24mohammadzadeh@gmail.com>
2022-08-23 16:22:49 +0430

    push

--- src/components/SelectOrigin/SelectOrigin.jsx
+++ src/components/SelectOrigin/SelectOrigin.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React , {useState} from 'react'
 import { Icon } from '@iconify/react';
 import './SelectOrigin.css'
+import countries from "i18n-iso-countries";
+import enLocale from "i18n-iso-countries/langs/en.json";
 
 function SelectOrigin({Closer}) {
-
   const closeSelect = () => {
     Closer(false)
   }
+  const [selectedCountry, setSelectedCountry] = useState("");
+
+  const selectCountryHandler = (value) => setSelectedCountry(value);
+
+  // Have to register the languages you want to use
+  countries.registerLocale(enLocale);
+
+  // Returns an object not a list
+  const countryObj = countries.getNames("en", { select: "official" });
+
+  const countryArr = Object.entries(countryObj).map(([key, value]) => {
+    return {
+      label: value,
+      value: key
+    };
+  });
 
   return (
     <div className='SelectOrigin-Container'>
@@ -21,7 +38,14 @@ function SelectOrigin({Closer}) {
         <span className='ch-txts'>Pleasse select your city.</span>
         <div className="OriginCity">
           <label>Origin city</label>
-          <input className='origin-input' type="text" />
+          <select className='origin-input' value={selectedCountry} onChange={(e) => selectCountryHandler(e.target.value)}>
+        {!!countryArr?.length &&
+          countryArr.map(({ label, value }) => (
+            <option key={value} value={value}>
+              {label}
+            </option>
+          ))}
+        </select>
           <span className='input-bottom-txt'>Enter your city name Please</span>
         </div>
         <button style={{ position: "absolute", bottom: "0", left: "0", }} className='Continue-btn'>Confirm</button>
