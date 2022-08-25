8b8bd48e131f7932cea0dfc30e2963d3a894df95
Ali M24 <ali24mohammadzadeh@gmail.com>
2022-08-25 15:34:46 +0430

    save

--- src/components/SelectOrigin/SelectOrigin.jsx
+++ src/components/SelectOrigin/SelectOrigin.jsx
@@ -1,30 +1,12 @@
 import React , {useState} from 'react'
 import { Icon } from '@iconify/react';
 import './SelectOrigin.css'
-import countries from "i18n-iso-countries";
-import enLocale from "i18n-iso-countries/langs/en.json";
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
+import CountryData from '../../CountruData.json'
 function SelectOrigin({Closer}) {
   const closeSelect = () => {
     Closer(false)
   }
-  const [selectedCountry, setSelectedCountry] = useState("");
-
-  const selectCountryHandler = (value) => setSelectedCountry(value);
-
-  // Have to register the languages you want to use
-  countries.registerLocale(enLocale);
-
-  // Returns an object not a list
-  const countryObj = countries.getNames("en", { select: "official" });
-
-  const countryArr = Object.entries(countryObj).map(([key, value]) => {
-    return {
-      label: value,
-      value: key
-    };
-  });
+  const [countries , setCountries] = useState(CountryData)
 
   return (
     <div className='SelectOrigin-Container'>
@@ -39,16 +21,16 @@ function SelectOrigin({Closer}) {
         <span className='ch-txts'>Pleasse select your city.</span>
         <div className="OriginCity">
           <label>Origin city</label>
-          <input className='origin-input' value={selectedCountry} onChange={(e) => selectCountryHandler(e.target.value)}>
+          <input className='origin-input'>
           <span className='input-bottom-txt'>Enter your city name Please</span>
-          <List>
+          {/* <List>
           {!!countryArr?.length &&
           countryArr.map(({ label, value }) => (
             <ListItem key={value} value={value}>
               {label}
             </ListItem>
           ))}
-          </List>
+          </List> */}
         </input>
         </div>
         <button style={{ position: "absolute", bottom: "0", left: "0", }} className='Continue-btn'>Confirm</button>
